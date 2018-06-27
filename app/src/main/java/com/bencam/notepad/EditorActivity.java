861af30a4f9bd18865a8a60be02395c4b8707657
fresh_prince <das16001@byui.edu>
2018-06-27 15:33:46 -0600

    Added new note activity.

---
+++ app/src/main/java/com/bencam/notepad/EditorActivity.java
@@ -0,0 +1,13 @@
+package com.bencam.notepad;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+    }
+}
